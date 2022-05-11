315ec480865d0a20731298e738696a14895a9d38
J Moore <89619763+JMoore910@users.noreply.github.com>
2022-05-11 00:23:32 -0400

    Update Solution09.java

--- exercise09/src/main/java/Solution09.java
+++ exercise09/src/main/java/Solution09.java
@@ -16,16 +16,25 @@ public class Solution09 {
          Rounds up to the next whole number of gallons needed
          using Math.ceil
          */
+        
+        //  Use a scanner to take user input and calculate the volume of paint to cover a surface area 
         Scanner input = new Scanner(System.in);
         int length,width,convert = 350;
         double area,gal;
+        
+        //  Takes in length and width of surface to paint
         System.out.printf("Please enter the length of surface to paint: ");
         length = parseInt(input.nextLine());
         System.out.printf("Please enter the width of surface to paint: ");
         width = parseInt(input.nextLine());
+        
+        //  Calculates surface area, and volume using a conversion constant
         area = width * length;
         gal = area / convert;
+        
+        //  Use ceil to round the volume up to the next unit, then output the volume required to paint the surface area
         gal = Math.ceil(gal);
-        System.out.println("You will need to purchase " + String.format("%.0f",gal) +" gallons of paint to cover "+ String.format("%.0f",area) +" square feet.");
+        System.out.println("You will need to purchase " + String.format("%.0f",gal)  +
+                           " gallons of paint to cover "+ String.format("%.0f",area) +" square feet.");
     }
 }
