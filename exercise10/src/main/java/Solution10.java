b0bb01fe48763883ecbc2c30708233065fb19f8e
J Moore <89619763+JMoore910@users.noreply.github.com>
2022-05-11 00:27:27 -0400

    Update Solution10.java

--- exercise10/src/main/java/Solution10.java
+++ exercise10/src/main/java/Solution10.java
@@ -20,21 +20,30 @@ public class Solution10 {
          -As per usual use parseInt to convert input strings to integers
          */
 
+        //  Use a scanner to take in the price and quantity of three separate items
         Scanner input = new Scanner(System.in);
         int [] quan = new int[3];
         int [] price = new int[3];
         double subTotal = 0,tax = 0.055,cartTax,newTot;
+        
+        //  use a for loop to take in three different prices and quantities
         for (int i = 0;i<3;i++){
             System.out.printf("Enter price of item "+(i+1)+": ");
             price[i] = parseInt(input.nextLine());
             System.out.printf("Enter quantity of item "+(i+1)+": ");
             quan[i] = parseInt(input.nextLine());
         }
+        
+        //  use another for loop to calculate the subtotal as a summation of each item price * quantity
         for (int i = 0;i<3;i++){
             subTotal += (price[i]*quan[i]);
         }
+        
+        //  Calculate the current tax of the user's cart as well as the new total with the tax included
         cartTax = subTotal * tax;
         newTot = subTotal + cartTax;
+        
+        //  Output the subtotal, tax, and total
         System.out.println("Subtotal: $" + String.format("%.2f",subTotal));
         System.out.println("Tax: $" + String.format("%.2f",cartTax));
         System.out.println("Total: $" + String.format("%.2f",newTot));
