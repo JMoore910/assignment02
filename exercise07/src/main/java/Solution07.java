257579f4b4828989dfc3cf3e6a036e877ddc29e9
J Moore <89619763+JMoore910@users.noreply.github.com>
2022-05-11 00:14:08 -0400

    Update Solution07.java

--- exercise07/src/main/java/Solution07.java
+++ exercise07/src/main/java/Solution07.java
@@ -19,18 +19,25 @@ public class Solution07 {
           -KEEP calculations separate from output
           -Use a constant to hold the conversion factor
          */
+        
+        //  Use a scanner to take room dimensions as input
         Scanner input = new Scanner(System.in);
         double areaM,convert = 0.09290304;
         int length,width,areaF;
+        
+        //  Take in length and width of a room in feet
         System.out.printf("What is the length of the room in feet? ");
         length = parseInt(input.nextLine());
         System.out.printf("What is the width of the room in feet? ");
         width = parseInt(input.nextLine());
+        
+        //  Output the dimensions back to the user then calculate the areas in square feet and meters for output
         System.out.println("You entered the dimensions of " + length + " feet by " + width + " feet.");
         System.out.println("The area is");
         areaF = length * width;
         areaM = areaF * convert;
 
+        //  Output the areas, format the square meter area to be a float with three decimal places
         System.out.println(areaF + " square feet");
         System.out.println(String.format("%.3f",areaM) + " square meters");
     }
